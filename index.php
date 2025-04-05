265520e0ccd8f285be77a5566c2870bdc90fbee4
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2025-04-05 22:42:18 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<!------ the banner section-->
+
+
+<section class="hero-banner">
+    <div class="overlay">
+      <div class="hero-content">
+        <h1>Welcome to CSH</h1>
+        <p>
+          At CuraSolace Health Sciences Centre we care for our patients, families and each other
+          through everyday actions, significant moments and exciting breakthroughs.
+        </p>
+        <a href="#" class="discover-btn">Discover more <span>›</span></a>
+      </div>
+    </div>
+  </section>
+
+
+
+<!----- the banner code end -->
+
+
+
+
+
+
+<!------- the features lsit. -->
+
+
+
+<section class="features-grid">
+    <div class="feature-item">
+      <i class="fas fa-map-marker-alt"></i>
+      <h3>Getting to the hospital</h3>
+      <p>CSH provides care, services and support at two different locations in downtown Australia & Canada.</p>
+      <a href="#">Plan your journey to CSH</a>
+    </div>
+    <div class="feature-item">
+      <i class="fas fa-hospital-alt"></i>
+      <h3>While you are here</h3>
+      <p>Make the most of your hospital stay and explore on-site amenities and services.</p>
+      <a href="#">Explore on-site offerings</a>
+    </div>
+    <div class="feature-item">
+      <i class="fas fa-procedures"></i>
+      <h3>Visiting a patient</h3>
+      <p>Visiting a patient is an important part of the care journey.</p>
+      <a href="#">Learn more</a>
+    </div>
+    <div class="feature-item">
+      <i class="fas fa-car"></i>
+      <h3>Parking around KHSC</h3>
+      <p>There are many options for parking around the sites that make up CSH.</p>
+      <a href="#">Parking options</a>
+    </div>
+    <div class="feature-item">
+      <i class="fas fa-hand-holding-medical"></i>
+      <h3>Departments, programs and clinics</h3>
+      <p>CSH has a number of different departments, programs, clinics and services.</p>
+      <a href="#">Get the full list here</a>
+    </div>
+    <div class="feature-item">
+      <i class="fas fa-users"></i>
+      <h3>Working at CSH</h3>
+      <p>We always strive to attract dynamic and dedicated people to join our team.</p>
+      <a href="#">Join our team!</a>
+    </div>
+    <div class="feature-item">
+      <i class="fas fa-dollar-sign"></i>
+      <h3>Pay my bill</h3>
+      <p>Need to make a payment to CSH for items or services?</p>
+      <a href="#">Pay your bill here</a>
+    </div>
+    <div class="feature-item special">
+      <i class="fas fa-biohazard"></i>
+      <h3>COVID-19 Information</h3>
+      <p>Get up to date information related to COVID-19 and protocols at CHS.</p>
+      <a href="#">Get the latest</a>
+    </div>
+  </section>
+  
