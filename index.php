91d0831b3d722d24682594e932f19109f70cd43d
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2025-04-06 12:14:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <section class="hero-banner">
     <div class="overlay">
-      <div class="hero-content">
+      <div class="hero-content-1">
         <h1>Welcome to CSH</h1>
         <p>
           At CuraSolace Health Sciences Centre we care for our patients, families and each other
@@ -14,9 +14,6 @@
     </div>
   </section>
 
-
-
-<!----- the banner code end -->
 
 
 
@@ -77,4 +74,385 @@
       <a href="#">Get the latest</a>
     </div>
   </section>
+
+
+
+
+
+  <!----- image diver section-->
   
+  <section class="hero-section">
+    <div class="hero-image">
+      <img src="/images /Website CTA.webp" alt="Medical technology screen" />
+      <div style="display: none;" class="image-text">This is<br>the place.</div>
+    </div>
+    <div class="hero-content">
+        <h2>This is the place.</h2>
+      <p>This is where you go when you need help, that cares for our region and where amazing people are transforming care, together.</p>
+      <p>This is one of Australia top care, research and teaching hospitals, making groundbreaking advancements in health care.</p>
+      <p>Wondering what else makes CSH "the place?"</p>
+      <a href="#" class="hero-button">Click here</a>
+    </div>
+  </section>
+  
+
+
+
+
+
+<!---- the purple health seection-->
+
+
+<section class="plus-section">
+<div class="plus-contianer">
+
+<div class="plus-min">
+    <i class="fa-solid fa-notes-medical" style="color: #ffffff; font-size: 150px;"></i>
+  
+</div>
+
+
+<div class="plus-min">
+      <h1>Accessing Your Health Care Information</h1>
+      <p>The Privacy Office ensures that the information contained in your <br>
+       medical record is protected. There are various reasons you may need <br>
+       access to your information and we are here to help.</p>
+
+      <div class="but-btn"><button>Access Your Information</button></div>
+</div>
+
+
+<div class="clearfix"></div>
+
+</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<!---- the beginning of  second  purple health seection-->
+
+<main class="plus-main">
+
+<section class="plus-section-2">
+    <div class="plus-contianer">
+    
+    <div class="plus-min">
+        <img src="/images /MicrosoftTeams-image.webp" width="380px" height="auto">
+      
+    </div>
+    
+    
+    <div class="plus-min">
+          <h1 style="color: black;">Choose the Right Place for the Right Care</h1>
+          <p style="color: black;">Choosing the right place for the right care can positively impact <br>
+            your health, improve the work experiences of care teams, and <br>
+            allow the overall health system to function at its best.<br>
+            We encourage you to read about some of the people working in <br>
+            the specialty of Urgent and Emergency Care and a few of the <br>
+            patients who have benefited from their skills.
+        </p>
+    
+          <div class="but-btn"><button>Learn More</button></div>
+    </div>
+    
+    
+    <div class="clearfix"></div>
+    
+    </div>
+    </section>
+    
+    </main>
+    
+    
+    
+    
+    <!---- the end second purple health end seection-->
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!------ people of CSH codes section -->
+
+<section class="news-section">
+    <div class="news-container">
+    
+    <h1>People of CSH</h1>
+    
+    <div class="news-border">
+    <img src="/images /cilare-social-worker.webp">
+    
+    <h2>People of KHSC: Clare Bowley
+        </h2>
+    <p>If we had to describe Clare Bowley in one  <br>
+        word, it would be memorable.But it’s not just one thing is<br>
+        social workers does that makes her iconic. it's<br>
+         everything she does - and has done since joining<br>
+         CuraSolace Health Sciences Centre (CSHSC) 34 years...
+    </p>
+    
+    
+    <a>Learn More <i class="fa-solid fa-chevron-right dropdown_icon"></i></a>
+    
+    </div>
+    
+    
+    <!---- the second our people  border -->
+    
+    <div class="news-border">
+        <img src="/images /people-at-office-2.webp">
+        
+        <h2>People of CSHSC: Amelia Indigo<br> </h2>
+        <p>Building a trusting relationship, grounded in compassion, respect <br>
+            and understanding, is a top priority for Amelia Indigo <br>
+            the patients and families he supports are living with substance use<br>
+            disorders.Amelia Indigo...
+        </p>
+        
+        
+        <a>Learn More <i class="fa-solid fa-chevron-right dropdown_icon"></i></a>
+        
+        </div>
+    
+    
+    
+        <!---- the third our people  border -->
+        
+    
+    
+        <div class="news-border">
+            <img src="/images /people-in-clinic-3.webp">
+            
+            <h2>People + Pup of CSHSC: Kaida and Doc</h2>
+            <p>Feel-good alert! This week's dose of happiness is brought to you<br>
+                by a volunteering duo, who've been making a paw -sitive impact at <br>
+                CuraSolace Health Science Centre(CSHSC) for the last six years <br>
+                Meet Kaida and one of the goodest boys we know, Doc
+                 
+            </p>
+            
+            
+            <a>Learn More <i class="fa-solid fa-chevron-right dropdown_icon"></i></a>
+            
+            </div>
+    
+    
+    <div class="clearfix"></div>
+    
+    
+    
+    </div>
+    </section>
+
+
+
+<!------ people of CSH codes end section -->
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!---- the news sections -->
+
+
+
+<section class="news-section">
+<div class="news-container">
+
+<h1>Latest News</h1>
+
+<div class="news-border">
+<img src="/images /TITP_neuro_website_1800x1200.webp">
+<br><span>April 3, 2025</span>
+<h2>Integrative Brain Tumor Program <br>
+    achieving best-in-class patient outcomes</h2>
+<p>CuraSolace Health Science Centre (CSHSC) is setting <br>
+    benchmarks in brain tumor care with its groundbreaking <br>
+    integrative Brain Tumor Program (IBTP), developed under the<br>
+    leadership of Dr. Teresa Purzner, a neurosurgeon, researcher, <br>
+    and entrepreneur...
+</p>
+
+
+<a>Read More <i class="fa-solid fa-chevron-right dropdown_icon"></i></a>
+
+</div>
+
+
+<!---- the second news border -->
+
+<div class="news-border">
+    <img src="/images /MM6_6473.webp">
+    <br><span>April 3, 2025</span>
+    <h2>CSH launches southeastern Australia <br> first PET-CT program</h2>
+    <p>CuraSolace Health Sciences Centre (CSHSC) has <br>
+        reached a major milestone n the advancement of <br>
+        medical imaging for southeastern Australia, Officially<br>
+        completing construction of the region’s first <br>
+        positron emission tomography-computed <br>
+        tomography PET-CTsuite. 
+    </p>
+    
+    
+    <a>Read More <i class="fa-solid fa-chevron-right dropdown_icon"></i></a>
+    
+    </div>
+
+
+
+    <!---- the third news border -->
+    
+
+
+    <div class="news-border">
+        <img src="/images /DESIGN - 2025 04 - PET CT feature (1)_0.webp">
+        <br><span>April 3, 2025</span>
+        <h2>Cutting-edge imaging, local impact </h2>
+        <p>Patients in southeastern Ontario now have access to <br>
+            of the most advanced diagnostic imaging tools <br>
+            available, following the launch of CuraSolace Health <br>
+            Sciences Centre’s (CSHSC) new PET-CT program. T<br>
+            The first of its kind in the region, this state-of...  
+        </p>
+        
+        
+        <a>Read More <i class="fa-solid fa-chevron-right dropdown_icon"></i></a>
+        
+        </div>
+
+
+<div class="clearfix"></div>
+
+
+
+</div>
+</section>
+
+
+
+
+
+
+
+
+<!---- the Working and Volunteering at KHSC section -->
+
+
+
+
+<main class="plus-main">
+
+    <section class="plus-section-2">
+        <div class="plus-contianer">
+        
+        <div class="plus-min">
+            <img src="/images /nurs-client.webp" width="380px" height="auto">
+          
+        </div>
+        
+        
+        <div class="plus-min">
+              <h1 style="color: black;">Working and Volunteering at CSH</h1>
+              <p style="color: black;">Seeking career opportunities to joining a passionate  <br>
+                and dedicated team? Interested in supporting your <br>
+                community and giving back?
+               
+            </p>
+        
+              <div class="but-btn"><button>Discover Opportunities</button></div>
+        </div>
+        
+        
+        <div class="clearfix"></div>
+        
+        </div>
+        </section>
+        
+        </main>
+        
+        
+ 
+
+
+
+
+
+        <!------- the second  features lsit. section -->
+
+
+
+<section class="features-grid">
+    <div class="feature-item">
+      <i class="fa-solid fa-parking"></i>
+      <h3>Where do I Park?</h3>
+      <p>whether you're here for long or short stay you have several parking options to choose from </p>
+      <a href="#">Information about Parking</a>
+    </div>
+    
+    <div class="feature-item">
+      <i class="fas fa-procedures"></i>
+      <h3>How do locate a patient or family member</h3>
+      <p>are you looking to find or get in touch with a patient here at the hospital 
+        There are different ways you can reach them
+      </p>
+      <a href="#">Learn more here</a>
+    </div>
+    <div class="feature-item">
+      <i class="fa-solid fa-map"></i>
+      <h3>Maps and directions</h3>
+      <p>Whether you are traveling by car or bus you
+        should have no trouble finding us. 
+      </p>
+      <a href="#">Find us</a>
+    </div>
+    <div class="feature-item">
+      <i class="fa-solid fa-link"></i>
+      <h3>Connect with us</h3>
+      <p>Need to reach a person or department at CSHSC ?
+        or provide feedback </p>
+      <a href="#">Find how</a>
+    </div>
+    
+   
+  </section>
